38f8dc00b3eef651edb24c14d06155af08df127c
Ethan Lee <hello@ethanl.ee>
2021-11-16 03:01:00 -0800

    Initial commit

---
+++ test.pb.js
@@ -0,0 +1,18 @@
+protocol ISO_2_Pass_Unilateral_Authentication_Over_CCF [Prover(Secret), Verifier(Secret)] {
+  step Challenge [Verifier -> Prover] {
+    const Nonce = nonce();
+    Prover.send({"Nonce": Nonce});
+  }
+
+  step Response [Prover -> Verifier] {
+    const Hash = hash(Verifier.Challenge.Nonce + Verifier.Id + Prover.Input.Secret);
+    Verifier.send({"Hash": Hash});
+  }
+  
+  step Verify [Verifier] {
+    const Hash = hash(Verifier.Challenge.Nonce + Verifier.Id + Verifier.Input.Secret);
+    return (Hash === Prover.Response.Hash);
+  }
+}
+
+ISO_2_Pass_Unilateral_Authentication_Over_CCF.Prover({});
